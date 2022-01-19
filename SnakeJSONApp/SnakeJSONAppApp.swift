1bac51fcfafa21bb3e649c8135f470ff26fc9072
Ege Sucu <egesucu@icloud.com>
2022-01-19 18:09:38 +0300

    Initial Commit

---
+++ SnakeJSONApp/SnakeJSONAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnakeJSONAppApp.swift
+//  SnakeJSONApp
+//
+//  Created by Ege Sucu on 19.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct SnakeJSONAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
